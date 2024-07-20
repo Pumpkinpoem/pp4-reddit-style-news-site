1ef6b174839966e4cc0838549158958e4df96bbc
Andreas Almen <andreas.almen@live.com>
2024-07-20 12:02:48 +0000

    Build news app with models, forms, views, and templates

--- news/models.py
+++ news/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django_summernote.fields import SummernoteTextField
+
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    content = SummernoteTextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
--- news/views.py
+++ news/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from .models import Post
+from .forms import PostForm
 
 # Create your views here.
+
+
+def index(request):
+    posts = Post.objects.all()
+    return render(request, 'news/index.html', {'posts': posts})
+
+@login_required
+def create_post(request):
+    if request.method == 'POST':
+        form = PostForm(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.author = request.user
+            post.save()
+            return redirect('index')
+    else:
+        form = PostForm()
+    return render(request, 'news/create_post.html', {'form': form})
